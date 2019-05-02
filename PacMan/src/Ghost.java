e96eba8a82fb0464d5b6bcb96264ccbf3d51191d
Shams Ansari <shamsahmedansari@gmail.com>
2019-05-01 20:26:52 -0700

    Added ghost class and gif

---
+++ PacMan/src/Ghost.java
@@ -0,0 +1,14 @@
+import info.gridworld.actor.Actor;
+
+import java.awt.*;
+
+public class Ghost extends Actor {
+
+    private Color color;
+
+    public Ghost(Color color) {
+        super();
+    }
+
+
+}
--- PacMan/src/Main.java
+++ PacMan/src/Main.java
@@ -1,7 +1,15 @@
+import info.gridworld.grid.BoundedGrid;
+import info.gridworld.grid.Location;
+import info.gridworld.world.World;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        BoundedGrid grid = new BoundedGrid(15, 15);
+        World world = new World<>(grid);
+        Ghost ghost = new Ghost();
+        ghost.putSelfInGrid(grid, new Location(0, 0));
+        world.show();
     }
 
 }

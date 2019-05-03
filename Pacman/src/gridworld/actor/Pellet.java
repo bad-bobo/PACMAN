bab758ca896b7f5c7042ed7e3c0cb6450c8ad6da
Shams Ansari <shamsahmedansari@gmail.com>
2019-05-02 18:23:31 -0700

    New

---
+++ Pacman/src/gridworld/actor/Pellet.java
@@ -0,0 +1,13 @@
+package gridworld.actor;
+
+import java.awt.*;
+
+
+public class Pellet extends Actor
+{
+    public Pellet()
+    {
+        super();
+        setColor( new Color( 247, 255, 160 ) );
+    }
+}
---
+++ Pacman/src/gridworld/actor/PowerPellet.java
@@ -0,0 +1,13 @@
+package gridworld.actor;
+
+import java.awt.*;
+
+
+public class PowerPellet extends Pellet
+{
+    public PowerPellet()
+    {
+        super();
+        setColor( new Color( 247, 255, 160 ) );
+    }
+}
